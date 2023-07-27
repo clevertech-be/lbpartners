4a401de9301efacbce84d31a412606d9cb4caf01
Rachid Asli <rachid@switchy.be>
2023-07-28 00:50:10 +0200

    Update translations, remove CTA, update GMaps, add testimonials

--- public/assets/js/content.js
+++ public/assets/js/content.js
@@ -119,57 +119,60 @@ jQuery(function ($) {
   }
 
   const translations = {};
-  translations.debug = {
-    "current-locale": "DEBUG",
+
+  translations.fr = {
+    "current-locale": "FR",
     navbar_text:
-      "Nos conseillers répondent à vos questions du lundi au vendredi, de 9h à 18h.",
+      "Nos conseillers répondent à vos questions du lundi au vendredi, de 9h à 17h.",
     menu_item1: "Accueil",
     menu_item2: "Services",
     menu_item3: "Équipe",
     menu_item4: "Témoignages",
     menu_item5: "Cas d'étude",
-
     languages_picker_title: "Choisissez une langue",
-
-    hero_item1_title: "Fiscalité des entreprises",
+    hero_item1_title: "Donnez vie à votre vision entrepreneuriale",
     hero_item1_body:
-      "Sed in orci nunc. Donec sit amet ante et dolor commodo venenatis vitae nec odio.",
-    hero_item2_title: "Conseils juridiques",
+      "Notre expertise en création d'entreprise transforme vos idées audacieuses en réalité concrète.",
+    hero_item2_title: "Faites grandir votre entreprise en toute confiance",
     hero_item2_body:
-      "Nos juristes vous fournissent assistance et conseil afin que votre entreprise puisse constamment répondre aux exigences légales et réglementaires en vigueur.",
-    hero_item3_title: "Gestion d'entreprise",
+      "Avec nos services en stratégie, développement et gestion des ressources humaines, votre croissance est entre de bonnes mains.",
+    hero_item3_title: "Assurez votre conformité et votre sérénité",
     hero_item3_body:
-      "Notre équipe vous accompagne et vous assiste lors de la création de votre entreprise et tout au long de votre activité.",
-    hero_item4_title: "Gestion du personnel",
+      "Nos services juridiques et fiscaux vous gardent en conformité et optimisent vos obligations pour une tranquillité d'esprit totale.",
+    hero_item4_title: "Saisissez les opportunités d'investissement",
     hero_item4_body:
-      "Nos spécialistes RH vous aident à recruter de nouveaux collaborateurs et s’occupent de la gestion salariale de votre entreprise en vous garantissant le respect de la législation sociale en vigueur.",
-
+      "Nos conseils en finances et investissements vous aident à identifier et à exploiter les opportunités les plus prometteuses.",
     services_pre_title: "Nos domaines d'expertise",
     services_title_beginning: "L'excellence dans",
     services_title_highlighted_end: "les services",
     services_body:
-      "Nous sommes spécialisés dans la prestation de services de conseil grâce à un ensemble de technologies de pointe et à une équipe de professionnels expérimentés. Voici un aperçu des options que vous pouvez engager.",
-    services_title_1: "Audit & Assurance",
-    services_body_1: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-    services_title_2: "Conseil financier",
-    services_body_2: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-    services_title_3: "Fiscalité",
-    services_body_3: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-    services_title_4: "Marketing",
-    services_body_4: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-    services_title_5: "Conseil juridique",
-    services_body_5: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-    services_title_6: "Gestion des risques",
-    services_body_6: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-
+      "Avec notre gamme complète de services, laissez-nous propulser votre entreprise vers de nouveaux sommets. Nous sommes spécialisés dans la prestation de services de conseil grâce à un ensemble de technologies de pointe et à une équipe de professionnels expérimentés. Voici un aperçu des options que vous pouvez engager.",
+    services_title_1: "Création d'entreprise",
+    services_body_1:
+      "Nous vous guidons pas à pas pour concrétiser votre projet entrepreneurial.",
+    services_title_2: "Audit et conseil",
+    services_body_2:
+      "Nos audits rigoureux et conseils personnalisés maximisent votre performance.",
+    services_title_3: "Stratégie et développement",
+    services_body_3:
+      "Nous définissons des stratégies d'entreprise robustes pour vous aider à évoluer.",
+    services_title_4: "Ressources humaines",
+    services_body_4:
+      "Optimisez la gestion de votre capital humain avec notre expertise.",
+    services_title_5: "Juridique et fiscal",
+    services_body_5:
+      "Restez en conformité et minimisez votre charge fiscale grâce à nos conseils avisés.",
+    services_title_6: "Finances et investissements",
+    services_body_6:
+      "Assurez la santé financière de votre entreprise et exploitez les opportunités d'investissement.",
     team_pre_title: "Notre équipe d'experts",
     team_title_beginning: "Rencontrez-",
     team_title_highlighted_end: "nous",
     team_body:
-      "L'éthique et l'intégrité sont les bases sur lesquels notre équipe repose. Ce sont des principes fondamentaux qui deviennent des attitudes quotidiennes.",
+      "À vos côtés pour une croissance durable. L’éthique et l'intégrité sont les bases sur lesquels notre équipe repose. Ce sont des principes fondamentaux qui deviennent des attitudes quotidiennes.",
     team_founders_speech_title: "Le mot des fondateurs",
     team_founders_speech_body:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ut lacinia diam.\n\nVivamus efficitur et est quis posuere. Nulla sollicitudin vulputate dui, id pretium tortor congue eu. Integer aliquet justo eu quam volutpat ullamcorper.\n\nDuis ut hendrerit mauris. Phasellus faucibus ut enim vel tincidunt. Nunc venenatis a dui et laoreet.",
+      "Depuis notre création, nous avons toujours cru en la puissance de l'entrepreneuriat. Notre mission chez Legal & Business Partners est d'aider chaque entrepreneur à réaliser son potentiel maximal. Nous comprenons les défis que représente la création et la gestion d'une entreprise, et c'est pourquoi nous proposons un accompagnement complet et personnalisé pour chaque étape de votre parcours entrepreneurial. Avec nous à vos côtés, vous pouvez vous concentrer sur ce que vous faites de mieux : innover, diriger et réussir.",
     team_founders_speech_signature: "Bilal & Jean-David",
     team_member_first_name_1: "Jean-David",
     team_member_last_name_1: "Lukandu",
@@ -177,14 +180,12 @@ jQuery(function ($) {
     team_member_first_name_2: "Bilal",
     team_member_last_name_2: "Ismail",
     team_member_job_title_2: "Fiscal advisor & co-founder",
-
     study_cases_pre_title: "Nos cas d'étude",
     study_cases_title_beginning: "Leurs projets,",
     study_cases_title_highlighted_end: "notre expertise",
     study_cases_body:
-      "Chaque mois, nous publions une étude de cas afin de vous faire découvrir comment notre expertise a contribué au développement et à la croissance de ceux qui nous ont fait confiance.",
-    study_cases_cta_more: "Nos cas d'étude",
-
+      "Découvrez comment notre expertise a contribué au développement et à la croissance de ceux qui nous ont fait confiance. Apprenez-en plus sur notre impact et nos réalisations à travers nos études de cas et actualités.",
+    study_cases_cta_more: "Découvrez nos cas d'étude",
     newsletter_pre_title: "Newsletter",
     newsletter_title_beginning: "Apprenez-en ",
     newsletter_title_highlighted_end: "plus",
@@ -193,176 +194,64 @@ jQuery(function ($) {
     newsletter_form_name_placeholder: "Nom",
     newsletter_form_email_address_placeholder: "Adresse e-mail",
     newsletter_form_cta_subscribe: "S'abonner",
-
     testimonials_pre_title: "Témoignages de nos clients",
     testimonials_title_beginning: "Ils nous font déjà",
     testimonials_title_highlighted_end: "confiance",
     testimonials_body:
       "Nous travaillons intensément à la recherche d'idéaux et de solutions qui peuvent avoir un effet positif sur la vie de nos clients. C'est ce qui nous anime et nous sommes sensibles à toute cette reconnaissance.",
-    testimonials_cta_more: "Nos cas d'étude",
-
+    testimonials_item1_text: "Temoignages 01",
+    testimonials_item1_name: "Prenom N.",
+    testimonials_item1_role: "Role/Position",
+    testimonials_item2_text: "Temoignages 02",
+    testimonials_item2_name: "Prenom N.",
+    testimonials_item2_role: "Role/Position",
+    testimonials_item3_text: "Temoignages 03",
+    testimonials_item3_name: "Prenom N.",
+    testimonials_item3_role: "Role/Position",
+    testimonials_item4_text: "Temoignages 04",
+    testimonials_item4_name: "Prenom N.",
+    testimonials_item4_role: "Role/Position",
+    testimonials_item5_text: "Temoignages 05",
+    testimonials_item5_name: "Prenom N.",
+    testimonials_item5_role: "Role/Position",
+    testimonials_item6_text: "Temoignages 06",
+    testimonials_item6_name: "Prenom N.",
+    testimonials_item6_role: "Role/Position",
+    testimonials_cta_more: "Découvrez nos cas d'étude",
     contact_pre_title: "Écrivez-nous",
     contact_title_beginning: "Dites-nous ",
     contact_title_highlighted_end: "tout",
     contact_body:
       "Dites-nous en plus sur votre situation et vos projets. Nous accorderons une attention particulière à votre message et vous répondrons dans les meilleurs délais.",
-
     contact_form_name_placeholder: "Nom",
     contact_form_email_address_placeholder: "Adresse e-mail",
     contact_form_phone_number_placeholder: "Numéro de téléphone",
     contact_form_message_placeholder: "Message",
     contact_form_subject: "Sujet",
-    contact_form_subject_option_1: "Audit & Assurance",
-    contact_form_subject_option_2: "Conseil financier",
-    contact_form_subject_option_3: "Fiscalité",
-    contact_form_subject_option_4: "Marketing",
-    contact_form_subject_option_5: "Conseil juridique",
-    contact_form_subject_option_6: "Gestion des risques",
+    contact_form_subject_option_1: "Création d'entreprise",
+    contact_form_subject_option_2: "Audit et conseil",
+    contact_form_subject_option_3: "Stratégie et développement",
+    contact_form_subject_option_4: "Ressources humaines",
+    contact_form_subject_option_5: "Juridique et fiscal",
+    contact_form_subject_option_6: "Finances et investissements",
     contact_form_subject_option_7: "Autre",
     contact_form_success: "Votre message a été envoyé avec succès. Merci.",
     contact_form_invalid:
       "Des erreurs de validation sont survenues. Veuillez confirmer les champs et soumettre à nouveau.",
     contact_form_error: "Sorry. Nous n'avons pas pu envoyer votre message.",
-
     contact_phone_number: "+32 2 522 08 13",
-    contact_email_address: "contact@lbpartners.be",
-    contact_office_address: "Av. Reine Astrid 59b", //Kon. Astridlaan 59b, 1780 Wemmel
-
+    contact_email_address: "info@lbpartners.be",
+    contact_office_address: "Av. Roi Albert I 64",
     document_privacy_policy: "Politique de confidentialité",
     document_terms_and_conditions: "Conditions générales d'utilisation",
     copyright: "© 2023 Legal and Business Partners SRL - Tous droits réservés.",
     backlink: "Fièrement réalisé par ",
-
     common_company_name: "Legal & Business Partners",
     common_legal_company_name: "Legal and Business Partners SRL",
     common_company_vat: "BE 0792.288.476",
-    common_company_address_street_and_house: "Av. Reine Astrid 59b",
+    common_company_address_street_and_house: "Av. Roi Albert I 64",
     common_company_address_city_country: "1780 Wemmel, Belgique",
     common_company_tagline: "Votre partenaire de confiance pour la croissance.",
-
-    common_cta_get_in_touch: "Prenez contact",
-    common_cta_more_info: "En savoir plus",
-    common_cta_view_map: "Afficher sur Google Maps ©",
-    common_cta_submit: "Soumettre",
-  };
-
-  translations.fr = {
-    "current-locale": "FR",
-    navbar_text:
-      "Nos conseillers répondent à vos questions du lundi au vendredi, de 9h à 18h.",
-    menu_item1: "Accueil",
-    menu_item2: "Services",
-    menu_item3: "Équipe",
-    menu_item4: "Témoignages",
-    menu_item5: "Cas d'étude",
-
-    languages_picker_title: "Choisissez une langue",
-
-    hero_item1_title: "Fiscalité des entreprises",
-    hero_item1_body:
-      "Sed in orci nunc. Donec sit amet ante et dolor commodo venenatis vitae nec odio.",
-    hero_item2_title: "Conseils juridiques",
-    hero_item2_body:
-      "Nos juristes vous fournissent assistance et conseil afin que votre entreprise puisse constamment répondre aux exigences légales et réglementaires en vigueur.",
-    hero_item3_title: "Gestion d'entreprise",
-    hero_item3_body:
-      "Notre équipe vous accompagne et vous assiste lors de la création de votre entreprise et tout au long de votre activité.",
-    hero_item4_title: "Gestion du personnel",
-    hero_item4_body:
-      "Nos spécialistes RH vous aident à recruter de nouveaux collaborateurs et s’occupent de la gestion salariale de votre entreprise en vous garantissant le respect de la législation sociale en vigueur.",
-
-    services_pre_title: "Nos domaines d'expertise",
-    services_title_beginning: "L'excellence dans",
-    services_title_highlighted_end: "les services",
-    services_body:
-      "Nous sommes spécialisés dans la prestation de services de conseil grâce à un ensemble de technologies de pointe et à une équipe de professionnels expérimentés. Voici un aperçu des options que vous pouvez engager.",
-    services_title_1: "Audit & Assurance",
-    services_body_1: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-    services_title_2: "Conseil financier",
-    services_body_2: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-    services_title_3: "Fiscalité",
-    services_body_3: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-    services_title_4: "Marketing",
-    services_body_4: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-    services_title_5: "Conseil juridique",
-    services_body_5: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-    services_title_6: "Gestion des risques",
-    services_body_6: "Lorem ipsum dolor sit amet consectetur adipiscing elit.",
-
-    team_pre_title: "Notre équipe d'experts",
-    team_title_beginning: "Rencontrez-",
-    team_title_highlighted_end: "nous",
-    team_body:
-      "L'éthique et l'intégrité sont les bases sur lesquels notre équipe repose. Ce sont des principes fondamentaux qui deviennent des attitudes quotidiennes.",
-    team_founders_speech_title: "Le mot des fondateurs",
-    team_founders_speech_body:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ut lacinia diam.\n\nVivamus efficitur et est quis posuere. Nulla sollicitudin vulputate dui, id pretium tortor congue eu. Integer aliquet justo eu quam volutpat ullamcorper.\n\nDuis ut hendrerit mauris. Phasellus faucibus ut enim vel tincidunt. Nunc venenatis a dui et laoreet.",
-    team_founders_speech_signature: "Bilal & Jean-David",
-    team_member_first_name_1: "Jean-David",
-    team_member_last_name_1: "Lukandu",
-    team_member_job_title_1: "Legal advisor & co-founder",
-    team_member_first_name_2: "Bilal",
-    team_member_last_name_2: "Ismail",
-    team_member_job_title_2: "Fiscal advisor & co-founder",
-
-    study_cases_pre_title: "Nos cas d'étude",
-    study_cases_title_beginning: "Leurs projets,",
-    study_cases_title_highlighted_end: "notre expertise",
-    study_cases_body:
-      "Chaque mois, nous publions une étude de cas afin de vous faire découvrir comment notre expertise a contribué au développement et à la croissance de ceux qui nous ont fait confiance.",
-    study_cases_cta_more: "Nos cas d'étude",
-
-    newsletter_pre_title: "Newsletter",
-    newsletter_title_beginning: "Apprenez-en ",
-    newsletter_title_highlighted_end: "plus",
-    newsletter_body:
-      "Grâce à nos newsletters, recevez nos cas d'étude, découvrez nos conseils en matière de gestion d'entreprise et gardez un oeil sur les dernières nouveautés en termes de fiscalité et législation.",
-    newsletter_form_name_placeholder: "Nom",
-    newsletter_form_email_address_placeholder: "Adresse e-mail",
-    newsletter_form_cta_subscribe: "S'abonner",
-
-    testimonials_pre_title: "Témoignages de nos clients",
-    testimonials_title_beginning: "Ils nous font déjà",
-    testimonials_title_highlighted_end: "confiance",
-    testimonials_body:
-      "Nous travaillons intensément à la recherche d'idéaux et de solutions qui peuvent avoir un effet positif sur la vie de nos clients. C'est ce qui nous anime et nous sommes sensibles à toute cette reconnaissance.",
-    testimonials_cta_more: "Nos cas d'étude",
-
-    contact_pre_title: "Écrivez-nous",
-    contact_title_beginning: "Dites-nous ",
-    contact_title_highlighted_end: "tout",
-    contact_body:
-      "Dites-nous en plus sur votre situation et vos projets. Nous accorderons une attention particulière à votre message et vous répondrons dans les meilleurs délais.",
-
-    contact_form_name_placeholder: "Nom",
-    contact_form_email_address_placeholder: "Adresse e-mail",
-    contact_form_phone_number_placeholder: "Numéro de téléphone",
-    contact_form_message_placeholder: "Message",
-    contact_form_subject: "Sujet",
-    contact_form_subject_option_1: "Audit & Assurance",
-    contact_form_subject_option_2: "Conseil financier",
-    contact_form_subject_option_3: "Fiscalité",
-    contact_form_subject_option_4: "Marketing",
-    contact_form_subject_option_5: "Conseil juridique",
-    contact_form_subject_option_6: "Gestion des risques",
-    contact_form_subject_option_7: "Autre",
-
-    contact_phone_number: "+32 2 522 08 13",
-    contact_email_address: "contact@lbpartners.be",
-    contact_office_address: "Av. Reine Astrid 59b", //Kon. Astridlaan 59b, 1780 Wemmel
-
-    document_privacy_policy: "Politique de confidentialité",
-    document_terms_and_conditions: "Conditions générales d'utilisation",
-    copyright: "© 2023 Legal and Business Partners SRL - Tous droits réservés.",
-    backlink: "Fièrement réalisé par ",
-
-    common_company_name: "Legal & Business Partners",
-    common_legal_company_name: "Legal and Business Partners SRL",
-    common_company_vat: "BE 0792.288.476",
-    common_company_address_street_and_house: "Av. Reine Astrid 59b",
-    common_company_address_city_country: "1780 Wemmel, Belgique",
-    common_company_tagline: "Votre partenaire de confiance pour la croissance.",
-
     common_cta_get_in_touch: "Prenez contact",
     common_cta_more_info: "En savoir plus",
     common_cta_view_map: "Afficher sur Google Maps ©",
